7e5e34ee9022f8985682149dcbad10d2363b955c
Mike_Went <git@meew.me>
2018-03-10 13:24:08 +0300

    Update rl-gw.py: style improvements
    part 2

--- rl-gw.py
+++ rl-gw.py
@@ -4,13 +4,13 @@ import requests
 import time
 
 class Telegram:
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+    def __init__(self, TOKEN):
+        self.TOKEN = TOKEN
+        self.api_url = "https://api.telegram.org/bot{}/".format(TOKEN)
 
     def get_updates(self, offset=None, timeout=3000):
         method = 'getUpdates'
-        params = {'timeout': timeout, 'offset' : offset}
+        params = {'timeout': timeout, 'offset': offset}
         answer = requests.get(self.api_url + method, params)
         answer2json = answer.json()['result']
         return answer2json 
@@ -18,7 +18,7 @@ class Telegram:
     def send_text(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
-        answer = requests.post( self.api_url + method, params )
+        answer = requests.post(self.api_url + method, params)
         return answer    
 
     def get_last_update(self):
@@ -33,77 +33,73 @@ class Telegram:
                 print(time.clock)
         return last_update
 
-#---
-#---
-#--- HARDCODED !!!
-token = ""
-telegram = Telegram(token)
-#- parse the nature of send data
-def parseTypeOfMessage( msgBuffer ):
-    print ( msgBuffer ) 
+TOKEN = ""
+telegram = Telegram(TOKEN)
+
+def parse_type_of_message(msg_buffer):
+    print(msg_buffer) 
     try:
-#- Types:
-        if 'text'      in msgBuffer[ 'message' ] :
-            msg =           msgBuffer[ 'message' ][ 'text' ]
-        elif 'sticker' in msgBuffer[ 'message' ] :      
-            msg =           msgBuffer[ 'message' ]['sticker'][ 'emoji' ]
-        elif 'document'   in msgBuffer[ 'message' ] :
-            msg =           'document'
-        elif 'audio'   in msgBuffer[ 'message' ] :
-            msg = 'audiofile: {0} -- {1}'.format(  msgBuffer[ 'message' ][ 'audio' ][ 'performer' ], msgBuffer[ 'message' ][ 'audio' ][ 'title' ] )
-        elif 'photo'   in msgBuffer[ 'message' ] :
-            msg =           'photo'
-        elif 'voice'   in msgBuffer[ 'message' ] :
-            msg =           'voice mail'
-        else :
+        # Types:
+        if 'text' in msg_buffer['message']:
+            msg = msg_buffer['message']['text']
+        elif 'sticker' in msg_buffer['message']:      
+            msg = msg_buffer['message']['sticker']['emoji']
+        elif 'document' in msg_buffer['message']:
+            msg = 'document'
+        elif 'audio' in msg_buffer['message']:
+            msg = 'audiofile: {0} -- {1}'.format(msg_buffer['message']['audio']['performer'], msg_buffer['message']['audio']['title'])
+        elif 'photo' in msg_buffer['message']:
+            msg = 'photo'
+        elif 'voice' in msg_buffer['message']:
+            msg = 'voice mail'
+        else:
             msg = 'unknown file'
     except KeyError:
             msg = 'unknown error' 
-#- special messages 
-    if 'forward_from_chat' in msgBuffer[ 'message' ] :
-            msg = '<forward from channel: {0}>: {1}'.format( msgBuffer[ 'message' ][ 'forward_from_chat' ][ 'title' ] , msg )
-    if 'forward_from' in msgBuffer[ 'message' ] :
-            msg = '< forward from: {0} >: {1}'.format( msgBuffer[ 'message' ][ 'forward_from' ][ 'username' ] , msg )
-    if 'reply_to_message' in msgBuffer[ 'message' ] :
-            msg = '< answer for @{0} > {1}'.format( msgBuffer[ 'message' ][ 'reply_to_message' ][ 'from' ][ 'username' ], msg )
+    # special messages 
+    if 'forward_from_chat' in msg_buffer['message']:
+            msg = '<forward from channel: {0}>: {1}'.format(msg_buffer['message']['forward_from_chat']['title'] , msg)
+    if 'forward_from' in msg_buffer['message']:
+            msg = '< forward from: {0} >: {1}'.format(msg_buffer['message']['forward_from']['username'] , msg)
+    if 'reply_to_message' in msg_buffer['message']:
+            msg = '< answer for @{0} > {1}'.format(msg_buffer['message']['reply_to_message']['from']['username'], msg)
     return msg
 
 def main():
-     parcedMsgId = None
-     log = open("./tg.log", "a")
-     while True:
-         log.write( '\n' )
-         log.flush()
-#-  1: offset, id of message to be parced; 2: get this msg 
-         telegram.get_updates( parcedMsgId )
-         msgBuffer  = telegram.get_last_update()
-#- parcing
-         msg_upd_id     = msgBuffer[ 'update_id' ]
-#- prepared for nxt itteration
-         parcedMsgId = msg_upd_id + 1
-#- edited text has own type
-         if 'edited_message' in msgBuffer :
-           emsg_chat_id = msgBuffer[ 'edited_message'][ 'chat' ][ 'id']
-           emsg_name    = msgBuffer[ 'edited_message' ][ 'from' ][ 'username' ]
-           emsg_text    = msgBuffer[ 'edited_message' ][ 'text' ]
-           r = '@{0} changed his message to "{1}" '.format( emsg_name,emsg_text)
-           telegram.send_text( emsg_chat_id, r )
-           log.write( r )
-           log.write( time.ctime( time.time() ) )
-           continue
-#-
-         msg_text       = parseTypeOfMessage( msgBuffer )
-         msg_chat_id    = msgBuffer[ 'message' ][ 'chat' ][ 'id' ]
-         msg_name       = msgBuffer[ 'message' ][ 'from' ][ 'username' ]
-#-
-         response   = '@{0} sent: {1} '.format( msg_name, msg_text )
-         telegram.send_text( msg_chat_id, response )
-         log.write( response )
-         log.write( time.ctime( time.time() ) )
-#---
+    parsed+msg_id = None
+    log = open("./tg.log", "a")
+    while True:
+        log.write('\n')
+        log.flush()
+        #  1: offset, id of message to be parced; 2: get this msg 
+        telegram.get_updates(parsed+msg_id)
+        msg_buffer = telegram.get_last_update()
+        # parcing
+        msg_upd_id = msg_buffer['update_id']
+        # prepared for nxt itteration
+        parsed+msg_id = msg_upd_id + 1
+        # edited text has own type
+        if 'edited_message' in msg_buffer:
+            emsg_chat_id = msg_buffer['edited_message']['chat']['id']
+            emsg_name = msg_buffer['edited_message']['from']['username']
+            emsg_text = msg_buffer['edited_message']['text']
+            r = '@{0} changed his message to "{1}" '.format(emsg_name,emsg_text)
+            telegram.send_text(emsg_chat_id, r)
+            log.write(r)
+            log.write(time.ctime(time.time()))
+            continue
+
+        msg_text = parse_type_of_message(msg_buffer)
+        msg_chat_id = msg_buffer['message']['chat']['id']
+        msg_name = msg_buffer['message']['from']['username']
+
+        response = '@{0} sent: {1} '.format(msg_name, msg_text)
+        telegram.send_text(msg_chat_id, response)
+        log.write(response)
+        log.write(time.ctime(time.time()))
+
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         exit()
-
